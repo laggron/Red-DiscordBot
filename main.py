d9ad3d050f67a72910142fa3661726deabf96822
retke <laggron42@gmail.com>
2017-08-22 13:50:59 +0200

    main.py translation

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import discord
 import sys
 
 if discord.version_info.major < 1:
-    print("You are not running the rewritten version of discord.py.\n\n"
-          "In order to use Red v3 you MUST be running d.py version"
+    print("Vous ne tournez pas la dernière version de discord.py.\n\n"
+          "Pour utiliser Red v3 vous DEVEZ tourner la dernière version de discord.py"
           " >= 1.0.0.")
     sys.exit(1)
 
@@ -81,7 +81,7 @@ def determine_main_folder() -> Path:
 
 async def _get_prefix_and_token(red, indict):
     """
-    Again, please blame <@269933075037814786> for this.
+    Il faut blamer <@269933075037814786> pour ceci.
     :param indict:
     :return:
     """
@@ -96,11 +96,11 @@ if __name__ == '__main__':
     if cli_flags.config:
         load_basic_configuration(Path(cli_flags.config).resolve())
     else:
-        warn("Soon you will need to change the way you load the bot."
-             " The new method of loading has yet to be decided upon but"
-             " will be made clear in announcements from the support server"
-             " and from documentation. Please see issue #938 for further"
-             " discussion on this topic.",
+        warn("Bientôt vous devrez changer la manière dont vous lancez le bot."
+             " La nouvelle méthode pour le lancer n'a pas étée décidée mais"
+             " sera rendue claire dans les annonces du serveur support"
+             " et de la documentation. Regardez l'issue #938 pour une"
+             " discussion à ce sujet.",
              category=FutureWarning)
         import core.data_manager
         defaults = core.data_manager.basic_config_default.copy()
@@ -139,7 +139,7 @@ if __name__ == '__main__':
             if new_token:
                 token = new_token
         else:
-            log.critical("Token and prefix must be set in order to login.")
+            log.critical("Le token et le préfix doivent être réglés avant la connexion")
             sys.exit(1)
 
     if enable_sentry is None:
@@ -156,23 +156,23 @@ if __name__ == '__main__':
         loop.run_until_complete(red.start(token, bot=not cli_flags.not_bot))
     except discord.LoginFailure:
         cleanup_tasks = False  # No login happened, no need for this
-        log.critical("This token doesn't seem to be valid. If it belongs to "
-                     "a user account, remember that the --not-bot flag "
-                     "must be used. For self-bot functionalities instead, "
+        log.critical("Ce token n'est pas valide. Si il appartient à "
+                     "un compte utilisateur, n'oubliez pas que le --bot-no flag "
+                     "doit être utilisé. Pour les fonctions de selfbot, utilisez plutot "
                      "--self-bot")
         db_token = red.db.token()
         if db_token and not cli_flags.no_prompt:
-            print("\nDo you want to reset the token? (y/n)")
+            print("\nVoulez vous réintitialiser le token ? (y/n)")
             if confirm("> "):
                 loop.run_until_complete(red.db.token.set(""))
-                print("Token has been reset.")
+                print("Le token à bien été réinitialité")
     except KeyboardInterrupt:
-        log.info("Keyboard interrupt detected. Quitting...")
+        log.info("Interription système détéctée. Fermeture...")
         loop.run_until_complete(red.logout())
         red._shutdown_mode = ExitCodes.SHUTDOWN
     except Exception as e:
-        log.critical("Fatal exception", exc_info=e)
-        sentry_log.critical("Fatal Exception", exc_info=e)
+        log.critical("Erreur fatale", exc_info=e)
+        sentry_log.critical("Erreue fatale", exc_info=e)
         loop.run_until_complete(red.logout())
     finally:
         if cleanup_tasks:
